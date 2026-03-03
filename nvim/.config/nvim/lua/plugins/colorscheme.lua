796b6da448cf032c2f21dea0f0cddf685664c988
thin6 <thin6@premiertech.com>
2026-03-03 10:13:31 -0500

    Updated neovim colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,31 @@
-return {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    opts = {
-        styles = {
-            transparency = true
-        }
-    },
+return
+-- {
+--     "rose-pine/neovim",
+--     name = "rose-pine",
+--     opts = {
+--         styles = {
+--             transparency = true
+--         }
+--     },
+--     config = function()
+--         vim.cmd.colorscheme("rose-pine")
+--         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--         vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+--
+--         -- Popups / menus
+--         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--         vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+--         vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+--     end
+-- }
+{
+    "metalelf0/black-metal-theme-neovim",
+    lazy = false,
+    priority = 1000,
     config = function()
-        vim.cmd.colorscheme("rose-pine")
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-
-        -- Popups / menus
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
-        vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
-    end
+        require("black-metal").setup({
+            theme = 'taake'
+        })
+        require("black-metal").load()
+    end,
 }
