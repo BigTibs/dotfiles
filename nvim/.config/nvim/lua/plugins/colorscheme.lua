00c178cd7a3a2dedd43861a273454de9893de0f3
thin6 <thin6@premiertech.com>
2026-03-04 17:12:06 -0500

    Added lackluster theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,24 @@
 return
--- {
+{
+    "slugbyte/lackluster.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        local lackluster = require("lackluster")
+        local color = lackluster.color -- blue, green, red, orange, black, lack, luster, gray1-9
+
+        lackluster.setup({
+            tweak_background = {
+                -- normal = 'none', -- transparent
+                normal = '#000000', -- hexcode
+                -- normal = '#ffffff', -- hexcode
+                -- normal = color.green,    -- lackluster color
+            },
+        })
+
+        vim.cmd.colorscheme("lackluster")
+    end
+} -- {
 --     "rose-pine/neovim",
 --     name = "rose-pine",
 --     opts = {
@@ -18,14 +37,14 @@ return
 --         vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
 --     end
 -- }
-{
-    "metalelf0/black-metal-theme-neovim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("black-metal").setup({
-            theme = 'taake'
-        })
-        require("black-metal").load()
-    end,
-}
+-- {
+--     "metalelf0/black-metal-theme-neovim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         require("black-metal").setup({
+--             theme = 'taake'
+--         })
+--         require("black-metal").load()
+--     end,
+-- }
